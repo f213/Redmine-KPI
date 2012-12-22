802cd299b75c6c02dd69d9c9e125e6bcedbef907
Fedor Borshev <fedor@shogo.ru>
2012-12-22 17:49:00 +0400

    Поддержка связанных задач - relatedFrom, relatedTo, related, children и parent. Все кроме последнего, это query::issues, последний это element::issue

--- lib/Redmine/KPI/Element/Issue.pm
+++ lib/Redmine/KPI/Element/Issue.pm
@@ -2,11 +2,11 @@ package Redmine::KPI::Element::Issue;
 use Badger::Class
 	base 		=> 'Redmine::KPI::Element::Base',
 	methods		=> {
-		_paramsToFetch	=> sub { qw /priority status author assignedTo start_date due_date done_ratio estimated_hours/ },
+		_paramsToFetch	=> sub { qw /priority status author assignedTo start_date due_date done_ratio estimated_hours relatedToMe relatedFromMe children relations parent/ },
 		timeEntries	=> sub { shift->__queryFactory('timeEntries', @_) },
 	},
-	mutators	=> qw /related/;
 ;
+use Redmine::KPI::Config; # we need this here because we are using $self->{queryFactory} directly sometimes, so we need to pass there config parameters
 
 our @FETCH_URL_PARAMETERS = qw /children relations changesets/;
 
@@ -39,6 +39,9 @@ sub _parse
 
 	$self->param('description',	$self->{rootNode}->findvalue('description'));
 
+	$self->__fetchParent();
+	
+	$self->__fetchRelations();
 
 	$self->__fetchChangeSets();
 	1;
@@ -59,6 +62,36 @@ sub __addStdParam
 }
 
 
+sub __fetchRelations
+{
+	my $self = shift;
+
+	my @toMe;
+	foreach($self->{rootNode}->findnodes('relations/relation[@issue_to_id = "' . $self->param('id') . '"]'))
+	{
+		push @toMe, $_->getAttribute('issue_id');
+	}
+	$self->param('relatedToMe', $self->{queryFactory}->query('issues', issue => \@toMe, passConfigParams($self->{config}))) if @toMe; #here we do not use method _queryFactory for bypassing its cache
+
+	my @fromMe;
+	foreach($self->{rootNode}->findnodes('relations/relation[@relation_type="relates" and @issue_id = "' . $self->param('id') . '"]'))
+	{
+		push @fromMe, $_->getAttribute('issue_to_id');
+	}
+	$self->param('relatedFromMe', $self->{queryFactory}->query('issues', issue => \@fromMe, passConfigParams($self->{config}), @_ )) if @fromMe;
+
+	my @children;
+
+	foreach($self->{rootNode}->findnodes('children/issue'))
+	{
+		push @children, $_->getAttribute('id');
+	}
+	$self->param('children', $self->{queryFactory}->query('issues', issue => \@children, passConfigParams($self->{config}), @_ )) if @children;
+
+	my @relates = (@fromMe, @toMe);
+	$self->param('relations', $self->{queryFactory}->query('issues', issue => \@relates, passConfigParams($self->{config}), @_ )) if @relates;
+
+}
 
 sub __fetchChangeSets
 {
@@ -76,12 +109,27 @@ sub __fetchChangeSets
 			user		=> $self->{elemFactory}->element('author',
 					id	=> $_->findvalue('user/@id'),
 					name	=> $_->findvalue('user/@name'),
+					passConfigParams($self->{config}),
+					@_,
 			),
 		);
 
 
 	}
 	$self->param('changesets', \%cs);
+}
+sub __fetchParent
+{
+	my $self = shift;
+
+	my $parentId = $self->{rootNode}->findvalue('parent/@id');
+
+	$self->param('parent', $self->{elemFactory}->element('issue',
+			id	=> $parentId,
+			passConfigParams($self->{config}),
+			@_,
+		)
+	);
 }
 sub __queryFactory
 {
