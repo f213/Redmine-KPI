6a8b07d7de0c9563913157a7193ccce9e82a3f13
Fedor Borshev <fedor@shogo.ru>
2013-01-07 18:23:35 +0400

    Fetcher теперь может нормально работать как с новыми, так и со старыми LWP::UserAgent

--- lib/Redmine/KPI/Fetch.pm
+++ lib/Redmine/KPI/Fetch.pm
@@ -14,7 +14,7 @@ sub fetch
 		confess('we need authKey param') if not $apiKey or not length $apiKey;
 		my $ua = new LWP::UserAgent;
 
-		$ua->ssl_opts(verify_hostname=>0, SSL_verify_mode => 0x00 ) if exists $param->{noVerifyHost} and $param->{noVerifyHost};
+		$ua->ssl_opts(verify_hostname=>0, SSL_verify_mode => 0x00 ) if exists $param->{noVerifyHost} and $param->{noVerifyHost} and $ua->can('ssl_opts'); # old versions of LWP::UserAgent dont need this. if it can not set ssl_options, it does not verify host name, AFAIK
 
 		$ua->default_header(
 			'X-Redmine-API-Key' => $apiKey,
