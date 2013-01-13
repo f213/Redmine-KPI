c0a743c1db592264cb15e86c9cdf05a9fff8b139
Fedor Borshev <fedor@shogo.ru>
2013-01-13 20:20:09 +0400

    Now project can fetch info too

--- lib/Redmine/KPI/Element/Project.pm
+++ lib/Redmine/KPI/Element/Project.pm
@@ -2,10 +2,25 @@ package Redmine::KPI::Element::Project;
 use Badger::Class
 	base		=> 'Redmine::KPI::Element::Base',
 	methods		=> {
+		_paramsToFetch  => sub { qw /name identifier description homepage/ },
 		issues		=> sub {shift->__queryFactory('issues', @_)},
 		timeEntries	=> sub {shift->__queryFactory('timeEntries', @_)},
 	},
 ;
+sub _getUrl
+{
+	my $self = shift;
+	my $url = 'projects/' . $self->param('id') . '.xml' if $self->param('id');
+}
+sub _parse
+{
+	# TODO - cover this with tests!
+	my $self = shift;
+	($self->{rootNode}) = $self->{xml}->findnodes('project');
+
+	$self->param($_, $self->{rootNode}->findvalue($_)) foreach qw /name identifier description homepage/;
+	1;
+}
 
 sub cost
 {
