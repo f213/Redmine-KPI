1fbdb38f750adc5442f850431e158b5b9b8abbf7
Fedor Borshev <fedor@shogo.ru>
2013-01-14 23:56:18 +0400

    Some Element::User params modifications - now it can give name again

--- lib/Redmine/KPI/Element/User.pm
+++ lib/Redmine/KPI/Element/User.pm
@@ -2,7 +2,7 @@ package Redmine::KPI::Element::User;
 use Badger::Class
 	base 	=> 'Redmine::KPI::Element::Base',
 	methods	=> {
-		_paramsToFetch		=> sub { qw /login firstname lastname mail/ },
+		_paramsToFetch		=> sub { qw /login name firstname lastname mail/ },
 		_paramsFromConfig	=> sub { qw /id name login firstname lastname mail admin/ }, # TODO - may be fetch 'admin' param somehow
 		timeEntries		=> sub { shift->__queryFactory('timeEntries', @_) },
 		issues			=> sub { my $self = shift; $self->_queryFactory('issues', author => $self->param('id'), @_) },
@@ -24,6 +24,8 @@ sub _parse
 	($self->{rootNode}) = $self->{xml}->findnodes('user');
 	
 	$self->__addStdTextParam($_) foreach qw /login firstname lastname mail/;
+
+	$self->param('name',	$self->param('firstname') . ' ' . $self->param('lastname'));
 	1;
 }
 
